b7e27239ee14922eb1e2cd2295e67723a2093f30
netovieira01_ <76048368+manoelvieira0@users.noreply.github.com>
2022-02-04 09:21:12 -0300

    Encapsulamento

--- conta.py
+++ conta.py
@@ -1,18 +1,22 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
         print("Depositado!")
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
         print("Sacado!")
+
+    def transferir(self, valor, destinatario):
+        self.sacar(valor)
+        destinatario.depositar(valor)
